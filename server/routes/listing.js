f85b9717155d36125c7cd9ef515673cd4a6c19b9
Joe Wales <joewales13@gmail.com>
2017-12-11 10:18:47 -0600

    cleaned up more comments

--- server/routes/listing.js
+++ server/routes/listing.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var Listing = require('../models/listing.schema');
 
 router.get('/', function(req, res){
-    // a get request for all games
     Listing.find({}, function(errorMakingDatabaseQuery, data){
         if (errorMakingDatabaseQuery) {
             console.log('error with game find', errorMakingDatabaseQuery);
@@ -16,7 +15,6 @@ router.get('/', function(req, res){
 
 
 router.post('/', function(req, res){
-    // a get request for all games
     var addProperty = new Listing(req.body);
 
     addProperty.save(function(errorMakingDatabaseQuery, data){

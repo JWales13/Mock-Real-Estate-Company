d717b3cc497f3afe70ffebf308c5a891796410f5
Joe Wales <joewales13@gmail.com>
2017-12-12 10:14:21 -0600

    change mongoose connect

--- server/modules/mongoose.connection.js
+++ server/modules/mongoose.connection.js
@@ -1,19 +1,17 @@
-function connect() {
-    var mongoose = require('mongoose');
-    var databaseURL = 'mongodb://localhost:27017/realestate';
+var mongoose = require('mongoose');
+var dbPath = 'mongodb://localhost:27017/realestate';
 
-    mongoose.connect(databaseURL);
+ 
   
     
     mongoose.connection.on('connected', function(){
-        console.log('mongoose connected to : ', databaseURL)
+        console.log('mongoose connected')
     })
     
     mongoose.connection.on('error', function(error){
-        console.log('mongoose connection error : ', error)
+        console.log('mongoose connection error : ')
     })
-}
 
-module.exports = {
-    connect: connect
-}
+mongoose.connect(dbPath)
+module.exports = 
+    mongoose

9e286bcd216e73a168198bdc9c3ec3653787e0b1
Joe Wales <joewales13@gmail.com>
2017-12-12 10:21:43 -0600

    fixed server

--- server/modules/mongoose.connection.js
+++ server/modules/mongoose.connection.js
@@ -1,17 +1,30 @@
 var mongoose = require('mongoose');
-var dbPath = 'mongodb://localhost:27017/realestate';
 
- 
-  
-    
-    mongoose.connection.on('connected', function(){
-        console.log('mongoose connected')
-    })
-    
-    mongoose.connection.on('error', function(error){
-        console.log('mongoose connection error : ')
-    })
+// Mongo Connection //
+var mongoURI = '';
+// process.env.MONGODB_URI will only be defined if you
+// are running on Heroku
+if(process.env.MONGODB_URI != undefined) {
+    // use the string value of the environment variable
+    mongoURI = process.env.MONGODB_URI;
+} else {
+    // use the local database server
+    mongoURI = 'mongodb://localhost:27017/databasename';
+}
 
-mongoose.connect(dbPath)
-module.exports = 
-    mongoose
+mongoose.connect(databaseUrl, {
+  useMongoClient: true
+});
+
+mongoose.connection.on('error', function(err){
+   if(err) {
+     console.log("MONGO ERROR: ", err);
+   }
+   res.sendStatus(500);
+});
+
+mongoose.connection.on('open', function(){
+   console.log("Connected to Mongo!");
+});
+
+module.exports = mongoose;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var rental = require('./routes/rental');
 var listing = require('./routes/listing');
-var mongooseConnection = require('./modules/mongoose.connection');
+require('./modules/mongoose.connection');
 
 
 var app = express();
@@ -12,7 +12,7 @@ app.use(express.static('server/public'));
 app.use(bodyParser.json()); // needed for angular requests
 
 /** ---------- DATABASE CONNECTION ---------- **/
-mongooseConnection.connect(); // module to spin up mongoose
+ // module to spin up mongoose
 
 /** ---------- EXPRESS ROUTES ---------- **/
 
